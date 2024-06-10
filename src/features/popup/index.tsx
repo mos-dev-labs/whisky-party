10d3b4d72dbb8702f42ef65804c34ed6fda8aa6b
KimHunJin <ysg01129@naver.com>
2024-06-10 21:17:36 +0900

    feat: Improve Modal UI

--- src/features/popup/index.tsx
+++ src/features/popup/index.tsx
@@ -75,6 +75,77 @@ export const MainPopup = ({status = false}) => {
                                         <span className="sr-only">Close</span>
                                         <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                                     </button>
+
+                                    <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+                                        <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
+                                            <img src={product.imageSrc} alt={product.imageAlt} className="object-cover object-center" />
+                                        </div>
+                                        <div className="sm:col-span-8 lg:col-span-7">
+                                            <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">{product.name}</h2>
+
+                                            <section aria-labelledby="information-heading" className="mt-2">
+                                                <h3 id="information-heading" className="sr-only">
+                                                    Product information
+                                                </h3>
+
+                                                <p className="text-2xl text-gray-900">{product.price}</p>
+
+                                                {/* Reviews */}
+                                                <div className="mt-6">
+                                                    <h4 className="sr-only">Reviews</h4>
+                                                    <div className="flex items-center">
+                                                        <div className="flex items-center">
+                                                            {[0, 1, 2, 3, 4].map((rating) => (
+                                                                <StarIcon
+                                                                    key={rating}
+                                                                    className={classNames(
+                                                                        product.rating > rating ? 'text-gray-900' : 'text-gray-200',
+                                                                        'h-5 w-5 flex-shrink-0'
+                                                                    )}
+                                                                    aria-hidden="true"
+                                                                />
+                                                            ))}
+                                                        </div>
+                                                        <p className="sr-only">{product.rating} out of 5 stars</p>
+                                                        <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                                                            {product.reviewCount} reviews
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </section>
+
+                                            <section aria-labelledby="options-heading" className="mt-10">
+                                                <h3 id="options-heading" className="sr-only">
+                                                    Product options
+                                                </h3>
+
+                                                <form>
+                                                    {/* Colors */}
+                                                    <fieldset aria-label="Choose a color">
+                                                        <legend className="text-sm font-medium text-gray-900">Color</legend>
+                                                    </fieldset>
+
+                                                    {/* Sizes */}
+                                                    <fieldset className="mt-10" aria-label="Choose a size">
+                                                        <div className="flex items-center justify-between">
+                                                            <div className="text-sm font-medium text-gray-900">Size</div>
+                                                            <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                                                                Size guide
+                                                            </a>
+                                                        </div>
+
+                                                    </fieldset>
+
+                                                    <button
+                                                        type="submit"
+                                                        className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                                    >
+                                                        Add to bag
+                                                    </button>
+                                                </form>
+                                            </section>
+                                        </div>
+                                    </div>
                                 </div>
                             </DialogPanel>
                         </TransitionChild>
