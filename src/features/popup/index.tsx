334522d50928cde73d849915f698c2af4c523448
KimHunJin <ysg01129@naver.com>
2024-06-11 23:59:43 +0900

    feat: Improve Modal UX

--- src/features/popup/index.tsx
+++ src/features/popup/index.tsx
@@ -1,157 +1,154 @@
-import {Fragment, useEffect, useState} from 'react'
-import { Dialog, DialogPanel, Radio, RadioGroup, Transition, TransitionChild } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
-import { StarIcon } from '@heroicons/react/20/solid'
+import {Dialog, DialogPanel, Transition, TransitionChild} from '@headlessui/react'
+import {XMarkIcon} from '@heroicons/react/24/outline'
+import {StarIcon} from '@heroicons/react/20/solid'
 
 const product = {
-    name: 'Basic Tee 6-Pack ',
-    price: '$192',
-    rating: 3.9,
-    reviewCount: 117,
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg',
-    imageAlt: 'Two each of gray, white, and black shirts arranged on table.',
-    colors: [
-        { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
-        { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
-        { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
-    ],
-    sizes: [
-        { name: 'XXS', inStock: true },
-        { name: 'XS', inStock: true },
-        { name: 'S', inStock: true },
-        { name: 'M', inStock: true },
-        { name: 'L', inStock: true },
-        { name: 'XL', inStock: true },
-        { name: 'XXL', inStock: true },
-        { name: 'XXXL', inStock: false },
-    ],
+  name: 'Basic Tee 6-Pack ',
+  price: '$192',
+  rating: 3.9,
+  reviewCount: 117,
+  href: '#',
+  imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-quick-preview-02-detail.jpg',
+  imageAlt: 'Two each of gray, white, and black shirts arranged on table.',
+  colors: [
+    {name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400'},
+    {name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400'},
+    {name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900'},
+  ],
+  sizes: [
+    {name: 'XXS', inStock: true},
+    {name: 'XS', inStock: true},
+    {name: 'S', inStock: true},
+    {name: 'M', inStock: true},
+    {name: 'L', inStock: true},
+    {name: 'XL', inStock: true},
+    {name: 'XXL', inStock: true},
+    {name: 'XXXL', inStock: false},
+  ],
 }
 
 // @ts-ignore
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ')
+}
+
+interface Props {
+  isShow: boolean
+  name?: string
+  image?: string
+  description?: string
+  alcohol?: number
+  onClose: () => void
 }
 
 
-export const MainPopup = ({status = false}) => {
-    const [open, setOpen] = useState(status)
+export const MainPopup = (props: Props) => {
 
-    useEffect(() => {
-        setOpen(status);
-    }, [status])
+  const { isShow, name, image, description, alcohol, onClose } = props
 
-    return (
-        <Transition show={open}>
-            <Dialog className="relative z-10" onClose={setOpen}>
-                <TransitionChild
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block" />
-                </TransitionChild>
+  return (
+    <Transition show={isShow}>
+      <Dialog className="relative z-10" onClose={onClose}>
+        <TransitionChild
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block"/>
+        </TransitionChild>
 
-                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                    <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
-                        <TransitionChild
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
-                            enterTo="opacity-100 translate-y-0 md:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 md:scale-100"
-                            leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
-                        >
-                            <DialogPanel className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
-                                <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-                                    <button
-                                        type="button"
-                                        className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
-                                        onClick={() => setOpen(false)}
-                                    >
-                                        <span className="sr-only">Close</span>
-                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                                    </button>
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
+            <TransitionChild
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+              enterTo="opacity-100 translate-y-0 md:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 md:scale-100"
+              leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+            >
+              <DialogPanel
+                className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
+                <div
+                  className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <button
+                    type="button"
+                    className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
+                    onClick={onClose}
+                  >
+                    <span className="sr-only">Close</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
+                  </button>
 
-                                    <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-                                        <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
-                                            <img src={product.imageSrc} alt={product.imageAlt} className="object-cover object-center" />
-                                        </div>
-                                        <div className="sm:col-span-8 lg:col-span-7">
-                                            <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">{product.name}</h2>
-
-                                            <section aria-labelledby="information-heading" className="mt-2">
-                                                <h3 id="information-heading" className="sr-only">
-                                                    Product information
-                                                </h3>
-
-                                                <p className="text-2xl text-gray-900">{product.price}</p>
-
-                                                {/* Reviews */}
-                                                <div className="mt-6">
-                                                    <h4 className="sr-only">Reviews</h4>
-                                                    <div className="flex items-center">
-                                                        <div className="flex items-center">
-                                                            {[0, 1, 2, 3, 4].map((rating) => (
-                                                                <StarIcon
-                                                                    key={rating}
-                                                                    className={classNames(
-                                                                        product.rating > rating ? 'text-gray-900' : 'text-gray-200',
-                                                                        'h-5 w-5 flex-shrink-0'
-                                                                    )}
-                                                                    aria-hidden="true"
-                                                                />
-                                                            ))}
-                                                        </div>
-                                                        <p className="sr-only">{product.rating} out of 5 stars</p>
-                                                        <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                                            {product.reviewCount} reviews
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </section>
-
-                                            <section aria-labelledby="options-heading" className="mt-10">
-                                                <h3 id="options-heading" className="sr-only">
-                                                    Product options
-                                                </h3>
-
-                                                <form>
-                                                    {/* Colors */}
-                                                    <fieldset aria-label="Choose a color">
-                                                        <legend className="text-sm font-medium text-gray-900">Color</legend>
-                                                    </fieldset>
-
-                                                    {/* Sizes */}
-                                                    <fieldset className="mt-10" aria-label="Choose a size">
-                                                        <div className="flex items-center justify-between">
-                                                            <div className="text-sm font-medium text-gray-900">Size</div>
-                                                            <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                                                Size guide
-                                                            </a>
-                                                        </div>
-
-                                                    </fieldset>
-
-                                                    <button
-                                                        type="submit"
-                                                        className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                                    >
-                                                        Add to bag
-                                                    </button>
-                                                </form>
-                                            </section>
-                                        </div>
-                                    </div>
-                                </div>
-                            </DialogPanel>
-                        </TransitionChild>
+                  <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+                    <div
+                      className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
+                      <img src={product.imageSrc} alt={product.imageAlt} className="object-cover object-center"/>
                     </div>
+                    <div className="sm:col-span-8 lg:col-span-7">
+                      <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">{product.name}</h2>
+
+                      <section aria-labelledby="information-heading" className="mt-2">
+                        <h3 id="information-heading" className="sr-only">
+                          Product information
+                        </h3>
+
+                        <p className="text-2xl text-gray-900">{product.price}</p>
+
+                        {/* Reviews */}
+                        <div className="mt-6">
+                          <h4 className="sr-only">Reviews</h4>
+                          <div className="flex items-center">
+                            <div className="flex items-center">
+                              {[0, 1, 2, 3, 4].map((rating) => (
+                                <StarIcon
+                                  key={rating}
+                                  className={classNames(
+                                    product.rating > rating ? 'text-gray-900' : 'text-gray-200',
+                                    'h-5 w-5 flex-shrink-0'
+                                  )}
+                                  aria-hidden="true"
+                                />
+                              ))}
+                            </div>
+                            <p className="sr-only">{product.rating} out of 5 stars</p>
+                            <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                              {product.reviewCount} reviews
+                            </a>
+                          </div>
+                        </div>
+                      </section>
+
+                      <section aria-labelledby="options-heading" className="mt-10">
+                        <h3 id="options-heading" className="sr-only">
+                          Product options
+                        </h3>
+
+                        <form>
+                          {/* Colors */}
+                          <fieldset aria-label="Choose a color">
+                            <legend className="text-sm font-medium text-gray-900">Color</legend>
+                          </fieldset>
+                          <button
+                            type="button"
+                            className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            onClick={onClose}
+                          >
+                            닫기
+                          </button>
+                        </form>
+                      </section>
+                    </div>
+                  </div>
                 </div>
-            </Dialog>
-        </Transition>
-    )
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  )
 }
--- src/widgets/main/body/index.tsx
+++ src/widgets/main/body/index.tsx
@@ -3,8 +3,9 @@ import {useState} from "react";
 import {MainPopup} from "@/features/popup";
 
 export const MainBody = () => {
-    const [open, setOpen] = useState(false)
-    const handleClick = () => setOpen(true)
+    const [isShow, setIsShow] = useState(false)
+    const handleClick = () => setIsShow(true)
+    const handlePopupClose = () => setIsShow(false)
 
     return (
         <div className="bg-white">
@@ -12,7 +13,7 @@ export const MainBody = () => {
                 <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
                     <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
                     <MainCards onClick={handleClick}/>
-                    <MainPopup status={open}/>
+                    <MainPopup isShow={isShow} onClose={handlePopupClose}/>
                 </div>
             </div>
         </div>
