2b6fb1f14c882cd3f9d9e5e7ccf477e79d55f72e
KimHunJin <ysg01129@naver.com>
2024-06-13 22:51:33 +0900

    feat: Implement document

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, {Head, Html, Main, NextScript} from "next/document";
+import React from "react";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Invite Whisky Party</title>
+          <meta name="description" content="위스키 파티에 초대합니다."/>
+          <link rel="icon" href="/logo.jpg"/>
+        </Head>
+        <Main/>
+        <NextScript/>
+      </Html>
+    )
+  }
+}
--- src/widgets/layouts/ui.tsx
+++ src/widgets/layouts/ui.tsx
@@ -2,7 +2,7 @@ import {PropsWithChildren} from "react";
 import './style.css'
 
 export const BaseLayout = ({children}: PropsWithChildren) => (
-    <div className="wrapper">
-        <main className="main">{children}</main>
-    </div>
+  <div className="wrapper">
+    <main className="main">{children}</main>
+  </div>
 )
